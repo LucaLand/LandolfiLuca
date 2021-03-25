2f98779f21a6904034a7afef98f0e92e3759da9d
LucaLand <luca.landolfi3@studio.unibo.it>
2021-03-25 14:10:20 +0100

    Added comments and fixed times

--- it.unibo.virtualrobotclient/app/src/main/java/it/unibo/resumablebw/AnotherObserver.java
+++ it.unibo.virtualrobotclient/app/src/main/java/it/unibo/resumablebw/AnotherObserver.java
@@ -8,31 +8,45 @@ public class AnotherObserver implements Observer, Runnable {
 
     private int stopCount = 0;
     @Override
-    public void update(Observable observable, Object o) {
+    public synchronized void update(Observable observable, Object o) {
         /*
+        Per constatare che una sleep in questa funzione
+        blocca l'interfaccia in quanto l'observer prende
+        il controllo del suo Thread
+         */
         try {
             System.out.println("AnotherObserver || update: 1 Blocco Interfaccia");
-            Thread.sleep(2000);
+            Thread.sleep(500);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        */
+
         System.out.println("AnotherObserver || update: 2 Sblocco Interfaccia");
         if(o.toString().equals("STOP")) {
+            stopCount++;
             new Thread(this).start();
         }
+        /*
+        Function return control to the UI Interface
+         while the run function is in execution on another Thread
+
+         */
     }
 
     @Override
     public void run() {
-            stopCount++;
-            System.out.println("AnotherObserver || THREAD run: StopCount = " + stopCount
+        /*
+        This part of the Observer will execute in concurrency not blocking the UI interface
+         */
+
+        System.out.println("AnotherObserver || THREAD run: StopCount = " + stopCount
                     + " || Active Threads:" + Thread.activeCount());
         try {
-            Thread.sleep(1000);
+            Thread.sleep(3500);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        System.out.println("AnotherObserver || THREAD run : FINISHED!");
+        System.out.println("AnotherObserver || THREAD run : FINISHED! " +
+                " || Thread n. " + (Thread.activeCount()-1));
     }
 }
